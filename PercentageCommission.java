5ddb3a6b49db4c6a03dd6e8f658f872989d97119
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-05-28 18:12:16 +0100

    Percentage Commissions Calculator

--- PercentageCommission.java
+++ PercentageCommission.java
@@ -6,23 +6,23 @@ public class PercentageCommission {
 
 	System.out.println("Type -1 to stop");
 	System.out.print("Enter your budget: ");
-	double userInput = input.nextDouble();
+	double amount = input.nextDouble();
 
-	double totalAmount = userInput;
+	double totalAmount = amount;
 	int numberOfItems = 0;
+	
+	while (amount != -1){
+	numberOfItems++;
+	System.out.print("Enter your budget: ");
+	amount = input.nextDouble();
+	totalAmount += amount;
+	}
 
 	double constantWeeklyCommission = 200;
 	double percentageCommission = (9.00 / 100);
 	double commissionFromSales = (totalAmount * percentageCommission);
 	double totalCommission = constantWeeklyCommission + commissionFromSales;
-	
-	while (userInput != -1){
-	numberOfItems++;
-	System.out.print("Enter your budget: ");
-	userInput = input.nextDouble();
-	totalAmount += userInput;
-	
-	}
-	System.out.printf("Your total commission for the week is $%.2f%nFor %d items.", totalCommission, numberOfItems);
+
+	System.out.printf("Your total commission for the week is $%.2f%nFor %d items.", 	totalCommission, numberOfItems);
 	}
 }
